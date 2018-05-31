46c6da36bb2966a52b32fb809d0df951214f5997
멀티1_06 <1_06@10.55.112.7>
2018-05-31 13:50:05 +0900

    한줄추가

--- src/main/java/kr/java/coffee/Main.java
+++ src/main/java/kr/java/coffee/Main.java
@@ -6,19 +6,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("마칠시간");
-		
+
 		Product p1 = new Product("A001");
 		Product p2 = new Product("A001");
-		
+
 		System.out.println(p1);
 		System.out.println(p2);
-	
-	
-	if(p1.equlas(p2)) {
-		System.out.println("같음");
-	}else {
-		System.out.println("다름");
-	}
+
+		if (p1.equlas(p2)) {
+			System.out.println("같음");
+		} else {
+			System.out.println("다름");
+		}
+		//이 부분은 Eclipse에서 추가
 	}
 
 }
